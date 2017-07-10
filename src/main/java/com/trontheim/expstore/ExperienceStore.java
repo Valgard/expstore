2881d9c06aefe25b1521c736ae8ed369e089682e
Sven Pöche <public@svenpoeche.de>
2017-07-10 15:47:56 +0200

    * push version to 0.0.4-alpha

--- src/main/java/com/trontheim/expstore/ExperienceStore.java
+++ src/main/java/com/trontheim/expstore/ExperienceStore.java
@@ -23,7 +23,7 @@ public class ExperienceStore {
 
   public static final String MODID = "expstore";
   public static final String NAME = "Experience Store";
-  public static final String VERSION = "0.0.3-alpha";
+  public static final String VERSION = "0.0.4-alpha";
   public static final String MCVERSION = "[1.7.10]";
 
   private static final Logger logger = LogManager.getLogger(MODID);
