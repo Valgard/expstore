55f19aaf46ae4f0deedf7e1170adb302104af753
Sven Pöche <public@svenpoeche.de>
2017-07-10 18:49:17 +0200

    * recipe registration moved to ESBlocks class

--- src/main/java/com/trontheim/expstore/common/proxy/CommonProxy.java
+++ src/main/java/com/trontheim/expstore/common/proxy/CommonProxy.java
@@ -4,29 +4,12 @@ import com.trontheim.expstore.init.ESBlocks;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.init.Blocks;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
-import net.minecraft.launchwrapper.Launch;
 
 abstract public class CommonProxy {
-
-  private static final boolean developmentEnvironment = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
 
   public void preInit(FMLPreInitializationEvent event) {
 
     ESBlocks.register();
-
-    GameRegistry.addRecipe(new ItemStack(ESBlocks.expStore), "ogo", "gGg", "ogo", 'o', Blocks.obsidian, 'g', Items.gold_ingot, 'G', Blocks.glass);
-
-    if(isDevelopmentEnvironment()) {
-      GameRegistry.addRecipe(new ItemStack(ESBlocks.expChanger), "ogo", "gGg", "ogo", 'o', Blocks.obsidian, 'g', Blocks.gold_block, 'G', Blocks.glass);
-    }
-  }
-
-  public static boolean isDevelopmentEnvironment() {
-    return developmentEnvironment;
   }
 
   public void init(FMLInitializationEvent event) {
--- src/main/java/com/trontheim/expstore/init/ESBlocks.java
+++ src/main/java/com/trontheim/expstore/init/ESBlocks.java
@@ -4,11 +4,15 @@ import com.trontheim.expstore.ExperienceStore;
 import com.trontheim.expstore.block.BlockExpChanger;
 import com.trontheim.expstore.block.BlockExpStore;
 import com.trontheim.expstore.block.itemblock.ItemBlockExpStore;
-import com.trontheim.expstore.client.renderer.block.RenderBlockExpStore;
 import com.trontheim.expstore.block.tileentity.TileEntityExpStore;
+import com.trontheim.expstore.client.renderer.block.RenderBlockExpStore;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.launchwrapper.Launch;
 import net.minecraft.tileentity.TileEntity;
 
 public class ESBlocks {
@@ -16,9 +20,28 @@ public class ESBlocks {
   public static final Block expStore = GameRegistry.registerBlock(new BlockExpStore(), ItemBlockExpStore.class, "expStoreBlock");
   public static final Block expChanger = GameRegistry.registerBlock(new BlockExpChanger(), "expChangerBlock");
 
+  private static final boolean developmentEnvironment = (Boolean) Launch.blackboard.get("fml.deobfuscatedEnvironment");
+
   public static void register() {
+    registerTileEntities();
+    registerRecipes();
+  }
+
+  private static void registerTileEntities() {
     TileEntity.addMapping(TileEntityExpStore.class, ExperienceStore.MODID + ":TileEntityExpStore");
     RenderingRegistry.registerBlockHandler(RenderBlockExpStore.instance());
+  }
+
+  private static void registerRecipes() {
+    GameRegistry.addRecipe(new ItemStack(ESBlocks.expStore), "ogo", "gGg", "ogo", 'o', Blocks.obsidian, 'g', Items.gold_ingot, 'G', Blocks.glass);
+
+    if(isDevelopmentEnvironment()) {
+      GameRegistry.addRecipe(new ItemStack(ESBlocks.expChanger), "ogo", "gGg", "ogo", 'o', Blocks.obsidian, 'g', Blocks.gold_block, 'G', Blocks.glass);
+    }
+  }
+
+  private static boolean isDevelopmentEnvironment() {
+    return developmentEnvironment;
   }
 
 }
